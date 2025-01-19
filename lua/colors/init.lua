f0f2383319b7b8b98166dbb8c7fb795fe28500e0
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-01-19 16:26:40 +0000

    Ok...

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -4,7 +4,7 @@ local buffer_utils = require("colors.buffer_utils")
 local colors = require("colors.color.utils")
 local color_patterns = require("colors.color.patterns")
 local ns_id = vim.api.nvim_create_namespace("colors")
-local bgc = require("colors.utils.create_highlight")
+local bgc = require("colors.utils")
 vim.notify(bgc, vim.log.levels.INFO)
 
 if vim.g.loaded_nvim_highlight_colors ~= nil then

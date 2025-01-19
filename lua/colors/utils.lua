b5230dfaae3e3f483f3fb06ba7d2172072c7ed46
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-01-19 17:00:30 +0000

    Ok this should work

--- lua/colors/utils.lua
+++ lua/colors/utils.lua
@@ -56,26 +56,34 @@ function M.create_highlight(active_buffer_id, ns_id, data, options)
 	end
 
 	local highlight_group = M.create_highlight_name(options.render .. data.value .. color_value)
+	local background_color = colors.get_background_color()
+	local reversed_background_color = colors.get_reversed_background_color()
 
 	if options.render == M.render_options.foreground then
-		local background_color = colors.get_background_color()
-		local reversed_background_color = colors.get_reversed_background_color()
-		--[[if background_color == color_value then
+		if background_color == color_value then
 			pcall(vim.api.nvim_set_hl, 0, highlight_group, {
-				fg = color_value,
-				bg = reversed_background_color,
+				fg = reversed_background_color,
+				bg = color_value,
 				default = true,
 			})
-		else ]]
+		else
 			pcall(vim.api.nvim_set_hl, 0, highlight_group, {
-				fg = color_value,
-				bg = background_color,
+				fg = background_color,
+				bg = color_value,
 				default = true,
 			})
-		--end
+		end
+	elseif options.render == M.render_options.virtual then
+		pcall(
+			M.highlight_extmarks,
+			active_buffer_id,
+			ns_id,
+			data,
+			highlight_group,
+			options
+		)
+		return
 	else
-		local background_color = colors.get_background_color()
-		local reversed_background_color = colors.get_reversed_background_color()
 		if background_color == color_value then
 			pcall(vim.api.nvim_set_hl, 0, highlight_group, {
 				fg = color_value,
@@ -91,17 +99,6 @@ function M.create_highlight(active_buffer_id, ns_id, data, options)
 		end
 	end
 
-	if options.render == M.render_options.virtual then
-		pcall(
-			M.highlight_extmarks,
-			active_buffer_id,
-			ns_id,
-			data,
-			highlight_group,
-			options
-		)
-		return
-	end
 	pcall(
 		function()
 			vim.api.nvim_buf_add_highlight(

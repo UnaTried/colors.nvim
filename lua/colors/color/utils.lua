cbae39076530344c3a93aa529ca6b2a88d3521d0
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-01-19 16:04:07 +0000

    Test

--- lua/colors/color/utils.lua
+++ lua/colors/color/utils.lua
@@ -201,7 +201,7 @@ function M.get_background_color()
 end
 
 -- Ensure bit32 module is available for Lua 5.1 (Lua 5.2+ uses the bit library by default)
-local bit = require('bit32')
+local bit = require('bit')
 
 function M.get_reversed_background_color()
     -- Get the 'Normal' highlight group properties

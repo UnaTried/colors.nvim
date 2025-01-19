4b2bb0a3cc91c3427bbca5ad904b26836327e8ec
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-01-19 16:46:32 +0000

    had to also remove smth

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -5,7 +5,6 @@ local colors = require("colors.color.utils")
 local color_patterns = require("colors.color.patterns")
 local ns_id = vim.api.nvim_create_namespace("colors")
 local u = require("colors.utils")
-vim.notify(u.bgc_rbgc, vim.log.levels.INFO)
 
 if vim.g.loaded_nvim_highlight_colors ~= nil then
 	return {}
--- lua/colors/utils.lua
+++ lua/colors/utils.lua
@@ -60,7 +60,6 @@ function M.create_highlight(active_buffer_id, ns_id, data, options)
 	if options.render == M.render_options.foreground then
 		local background_color = colors.get_background_color()
 		local reversed_background_color = colors.get_reversed_background_color()
-		local bgc_rbgc = { "background color: ", background_color, "reversed background color: ", reversed_background_color }
 		if background_color == color_value then
 			pcall(vim.api.nvim_set_hl, 0, highlight_group, {
 				fg = color_value,
